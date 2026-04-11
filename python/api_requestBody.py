a64bb8dcc45bb357517bac775ef4e469714515d6
Aryan. <79081443+Master-Gamer-glitch@users.noreply.github.com>
2026-04-11 09:26:14 +0000

    Learnt request body and validation

---
+++ python/api_requestBody.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+#BaseModel validates the data received and throws error otherwise.
+class Item(BaseModel):
+    name: str
+    price: float
+
+@app.post("/items")
+def create_item(item: Item):
+    return item
