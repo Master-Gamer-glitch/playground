8376cd7f92a4cb674050f4b9d6d27e610912bebf
Aryan. <79081443+Master-Gamer-glitch@users.noreply.github.com>
2026-05-20 13:04:31 +0530

    Implement SQL transaction logic for accounts
    Added SQL commands for account transactions and handling in Mongoose.

--- Js/transactions-using-mongoose.js
+++ Js/transactions-using-mongoose.js
@@ -19,3 +19,32 @@ try {
 }
 
 // NO SAVEPOINT FACILITY WITH MONGOOSE... HENCE ALTERNATIVE: "POOL" [NOT GONNA FAFO HERE];
+// MYSQL EQUIVALENT: 
+
+CREATE TABLE Accounts (
+  acc_id int primary key,
+  holder_name varchar(20),
+  balance float
+);
+
+insert into Accounts VALUES (1, "Rahul", 5000);
+insert into Accounts VALUES (2, "Priya", 7000);
+insert into Accounts VALUES (3, "Amoan", 9000);
+
+select * from Accounts;
+
+start transaction;
+update Accounts set balance= balance-1000 where acc_id=1;
+savepoint sp1;
+
+update Accounts set balance= balance+1000 where acc_id=2;
+savepoint sp2;
+
+rollback; -- Transaction hua and reverse hogya; [till last commit or pura]: ATOMICITY;
+rollback to savepoint sp2;
+
+-- commit ke upar nahi jaa skte. EK BAAR COMMIT KRDIA: MATLAB SAID, DONE, SEALED;
+
+-- end transaction; // DNE
+
+select * from Accounts;
