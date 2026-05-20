4d8e2de0c9381ced45fadbf77adcfedab96e2a41
Aryan. <79081443+Master-Gamer-glitch@users.noreply.github.com>
2026-05-20 12:47:04 +0530

    Handle transaction errors and resource cleanup
    Ensure transaction is aborted on error and resources are released.

---
+++ Js/transactions-using-mongoose.js
@@ -0,0 +1,18 @@
+//Learnt about transactions
+require("mongoose");
+
+const session = await mongoose.startSession();
+session.startTransaction();
+
+try {
+  await User.updateOne({ name: 'Alice' }, { $inc: { balance: -10 } }, { session });
+  await User.updateOne({ name: 'Bob' }, { $inc: { balance: 10 } }, { session });
+  await session.commitTransaction();
+
+} catch (error) {
+  await session.abortTransaction(); // atomicity;
+  
+} finally {
+  session.endSession(); //release resources;
+  
+}
