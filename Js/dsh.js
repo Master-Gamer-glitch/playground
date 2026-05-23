fb51088a8aae35fff76242ffb9618a4506f29f2a
Aryan. <79081443+Master-Gamer-glitch@users.noreply.github.com>
2026-05-23 22:23:28 +0530

    teaching nodemon, api, postman, basics.js

---
+++ Js/dsh.js
@@ -0,0 +1,52 @@
+const express = require("express");
+// npm i express
+// npm i nodemon
+// npm i mongoose
+// frontend => data chahiye => calls my api => express
+const app = express();
+const userSchema = require("./users");
+const mongoose = require("mongoose");
+
+// call back fn!!!!!!!!!! aisa fn jo tab call ho in a given condition...
+
+//middleware: converts incoming data into json
+app.use(express.json()); 
+
+app.get("/", function(request, response){
+    // db data variable 
+    response.status(404);
+    return response.send({message: "Hey Guyz!"}); //response body 
+    // "json.stringify"
+});
+
+users = ["sachin", "Dhruv", "Shaash", "hasini"] // database se aayegi
+
+app.get("/userdata", (req,dhruv) => {
+    dhruv.send(users);
+})
+
+app.post("/newUser", (req,res)=>{
+    // schema {name, age, email, password}
+    users = req.body;
+    res.send("data gaya");
+    render();
+})
+
+function render(){
+    console.log(users);
+    console.log(typeof users);
+}
+
+app.listen(8080, () => console.log("Hi backend!"));
+
+mongoose.connect("mongodb+srv://aryanshrivastava3005_db_user:BC1bOjt7gDO0PzQl@cluster0.b45ay9z.mongodb.net/")
+.then(console.log("database connected"))
+.catch(function(err){
+    console.log("ERROR in db connection " + err.message);
+}); // callback fn.........
+
+// restaurant => "menu" all list of apis available
+// waiter => kitchen to u... waiter that api transfer [api call]
+// kitchen => backend
+// api/http request type: get, post, put and delete: CRUD opertion, create, read, update and delete...
+// get post put delete
